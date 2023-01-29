8cb7adf17685229e184237d3d886bd8847d443c9
Bobby <m.babayi574@gmail.com>
2023-01-29 20:52:20 +0330

    fix crawler.py bugs

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -1,14 +1,22 @@
 # this is a crawler that crawls "https://www.banimode.com/new-products?sort|default=asc"
+# TODO : 
+
 
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+
 import time
 import random
 import pymongo
 # use multithreading to speed up the process
 import threading
-import concurrent.futures
+# import Queue and Thread to use multithreading
+from queue import Queue
+from threading import Thread
+# import WebDriverWait to wait for the page to load
+from selenium.webdriver.support.ui import WebDriverWait
+
 
 # get the products from the html
 def get_products(driver):
@@ -25,7 +33,9 @@ def get_products(driver):
             # object['productBrand'] is in "div[@class='product-card']/p/a/span[@class='product-card-brand']").text
             object['productBrand'] = item.find_element(By.XPATH,"div[@class='product-card']/p/a/span[@class='product-card-brand']").text
             # object['price'] is in "div[@class='product-card']//span[@class='price-disgit']").text
-            object['price'] = item.find_element(By.XPATH,"div[@class='product-card']//span[@class='price-disgit']").text
+            price = item.find_element(By.XPATH,"div[@class='product-card']//span[@class='price-disgit']").text
+            # price is a string be like "893,000" so we need to remove the comma and convert it to int
+            object['price'] = int(price.replace(",",""))
             # object['imageHide'] is in "div[@class='product-card']//img[@class='product-card-img hover-hide']").get_attribute("src")
             object['imageHide'] = item.find_element(By.XPATH,"div[@class='product-card']//img[@class='product-card-img hover-hide']").get_attribute("src")
             # object['imageShow'] is in "div[@class='product-card']//img[@class='product-card-img hover-show']").get_attribute("src")
@@ -36,9 +46,14 @@ def get_products(driver):
             continue
         # object['productDiscount'] is in "div[@class='product-card']//span[@class='product-card-discount']").text if it exists
         try:
-            object['productDiscount'] = item.find_element(By.XPATH,"div[@class='product-card']//span[@class='product-card-discount']").text
+            productDiscount = item.find_element(By.XPATH,"div[@class='product-card']//span[@class='product-card-discount']").text
+            # productDiscount is a string be like "20%" so we need to remove the "%" and convert it to int
+            object['productDiscount'] = int(productDiscount.replace("٪",""))
+            object['isDiscount'] = True
+            
         except:
             object['productDiscount'] = 0
+            object['isDiscount'] = False
         # object['productOldPrice'] is in "div[@class='product-card']//span[@class='product-card-lastprice']").text if it exists
         try:
             object['productOldPrice'] = item.find_element(By.XPATH,"div[@class='product-card']//span[@class='product-card-lastprice']").text
@@ -75,11 +90,39 @@ def store_products(mycol , products):
         else:
             mycol.insert_one(item)
 
+# initialize the driver
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--incognito')
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--disable-gpu')
+options.add_argument('--disable-extensions')
+options.add_argument('--disable-software-rasterizer')
+options.add_argument('--disable-browser-side-navigation')
+options.add_argument('--disable-features=VizDisplayCompositor')
+options.add_argument('--disable-features=NetworkService')
+options.add_argument('--disable-features=IsolateOrigins,site-per-process')
+options.add_argument('--disable-features=site-per-process')
+driver = webdriver.Chrome(options=options)
+
+# driver timeout is 10 seconds
+driver.set_page_load_timeout(10)
+
+# if timed out from the website, try again
+
 def make_driver(url):
-    options = webdriver.ChromeOptions()
-    options.add_argument('headless')
-    driver = webdriver.Chrome(options=options)
-    driver.get(url)
+    global driver
+    # if timed out from the website, try again
+    try:
+        # wait until the page load completely
+        driver.get(url)
+        # for prevent ban from the website wait [0,2) seconds
+        time.sleep(random.randint(0,2))
+    except:
+        print("timed out from the website, try again")
+        driver = make_driver(url)
     return driver
 
 
@@ -97,45 +140,39 @@ url = "https://www.banimode.com/new-products?sort|default=asc"
 driver = make_driver(url)
 
 last_page_number = find_last_page_number(driver)
-urls = ["https://www.banimode.com/new-products?sort|default=asc" + str(i) for i in range(1,last_page_number+1)]
+urls = ["https://www.banimode.com/new-products?sort|default=asc&page=" + str(i) for i in range(2,last_page_number+1)]
 
 # add products to mongodb database
 store_products(mycol , get_products(driver))
 
 # print the page number
-print("Page 1 is done")
+print("First Page Work is done")
 
 # speed up the process by using multithreading
 
 # make multi drivers for multithreading
 
 
-# Run store_products five by five for urls
-for i in range(0, len(urls), 5):
-    # make threads
-    threads = []
-    for j in range(i, i+5):
-        t = threading.Thread(target=store_products, args=(mycol, get_products(make_driver(urls[j]))))
-        threads.append(t)
-        t.start()
-    for t in threads:
-        t.join()
-    # print the page number
-    print("Page " + str(i+1) + " is done")
-
-    # for testing purposes stop after i > 20
-    if i > 20:
-        break
+# run the process for urls[:6]
+for url in urls[:20]:
+    driver = make_driver(url)
+    store_products(mycol , get_products(driver))
+    print("Page " + str(urls.index(url) + 2) + " Work is done")
 
 
+# test if all of products are stored in the database
+print(mycol.count_documents({}))
 
+print("All Pages Work is done")
 
 # close the driver
-driver.close()
+driver.quit()
+# close the mongodb client
+myclient.close()
 
 
-# print the number of products in the database
-print("Number of products in the database: " + str(mycol.count_documents({})))
+# TODO : use BeautifulSoup for scraping
+# TODO : use multithreading for scraping
 
 
 
