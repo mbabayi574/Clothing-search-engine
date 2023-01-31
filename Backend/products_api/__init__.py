0d31346aa9f7a87a4ddaecf07e4835cd012d40f1
Bobby <m.babayi574@gmail.com>
2023-01-31 15:28:18 +0330

    implement caching with redis

--- Backend/products_api/__init__.py
+++ Backend/products_api/__init__.py
@@ -1,10 +1,22 @@
 from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
+#  implement Redis cache for flask Api
+from flask_redis import FlaskRedis
+
+
 
 app = Flask(__name__)
 api = Api(app)
+# Resolve CORS problem
 CORS(app, support_credentials=True)
+
+# Redis configuration
+app.config['REDIS_URL'] = "redis://localhost:6379/0"
+# Initialize Redis
+redis_store = FlaskRedis(app)
+
+
 
 
 from .products import Products
